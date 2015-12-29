ac686ed84db2d235e5b237b89b8310041a013fa3
Scott Woods <scott@westarete.com>
2015-12-29 16:20:29 -0500

    Use dotenv to load settings

--- lib/tarsnap_pruner.rb
+++ lib/tarsnap_pruner.rb
@@ -1,4 +1,21 @@
 require "tarsnap_pruner/version"
 require "tarsnap_pruner/archive"
 require "tarsnap_pruner/scheduler"
-require "tarsnap_pruner/tarsnap_command"
+require "tarsnap_pruner/tarsnap"
+require "tarsnap_pruner/machine"
+
+require 'dotenv'
+
+module TarsnapPruner
+
+  module_function
+
+  def configure(config_file=nil)
+    if config_file
+      Dotenv.load(config_file)
+    else
+      Dotenv.load
+    end
+  end
+
+end
